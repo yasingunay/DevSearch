7b8cd8c930485d121dbd4119595999aeb5358c9f
yasingunay <av.yasingunay@gmail.com>
2023-11-21 12:19:06 +0100

    login and register added

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 
     path('', views.profiles, name="profiles"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
--- users/views.py
+++ users/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 from projects.models import Project
 
 
 def loginUser(request):
+
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -37,6 +40,29 @@ def logoutUser(request):
     logout(request)
     messages.success(request, 'User was logged out!')
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # does not save it to the database immediately
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, 'An error has occured during registration!')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
