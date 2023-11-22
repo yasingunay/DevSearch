98b1c5b60831ce28224a4021179b0645cfaf1340
yasingunay <yasingunaydev@gmail.com>
2023-11-22 15:15:33 +0100

    add-edit-delete added

--- projects/views.py
+++ projects/views.py
@@ -17,13 +17,16 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES )
         if form.is_valid():
-            form.save()
-            return redirect('projects')
+            project = form.save(commit=False) # does not save it to the database immediately
+            project.owner  = profile
+            project.save()
+            return redirect('account')
 
 
 
@@ -33,14 +36,15 @@ def createProject(request):
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id = pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id = pk)
     form = ProjectForm(instance = project)
 
     if request.method == 'POST':
         form = ProjectForm(request.POST,request.FILES,instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
 
 
 
@@ -50,10 +54,11 @@ def updateProject(request, pk):
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id = pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id = pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
 
     context = {'object': project}
-    return render(request, "projects/project_template.html", context)
+    return render(request, "delete_template.html", context)
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.models import User
 from .models import Profile
 from projects.models import Project
@@ -108,3 +108,54 @@ def editAccount(request):
 
     context = {'form': form}
     return render(request, 'users/profile_form.html' ,context)
+
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile =request.user.profile
+    form = SkillForm()
+
+    if request.method == "POST":
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill = form.save(commit=False)
+            skill.owner =profile
+            skill.save()
+            messages.success(request, 'Skill was added successfully!')
+            return redirect('account')
+        
+    context ={'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk):
+    profile =request.user.profile
+    skill = profile.skill_set.get(id = pk)
+    form = SkillForm(instance= skill)
+
+    if request.method == "POST":
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Skill was updated successfully!')
+            return redirect('account')
+        
+    context ={'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+
+
+@login_required(login_url='login')
+def deleteSkill(request, pk):
+    profile =request.user.profile
+    skill = profile.skill_set.get(id = pk)
+    if request.method == "POST":
+        skill.delete()
+        messages.success(request, 'Skill was deleted successfully!')
+        return redirect('account')
+
+    context ={'object': skill}
+    return render(request, 'delete_template.html', context)
